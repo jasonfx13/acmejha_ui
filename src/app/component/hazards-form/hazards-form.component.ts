5b3199aba71f3dfed124f6461c964607f88b7f64
Jason Fisher <jasonfx13@gmail.com>
2023-05-24 11:19:47 -0400

    Reactive Form reinit

---
+++ src/app/component/hazards-form/hazards-form.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-hazards-form',
+  templateUrl: './hazards-form.component.html',
+  styleUrls: ['./hazards-form.component.scss']
+})
+export class HazardsFormComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
